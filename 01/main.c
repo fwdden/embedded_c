a233e1fae0e2b104547c14f6326d3a3a8a044209
Denis Dubov <freeway.dbv@gmail.com>
2023-12-23 22:54:03 +0300

    01 done

---
+++ 01/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // 1
+    printf("Hello world!\n");
+
+    // 2
+    printf("Let's\n\tgo\n\t\tto walk");
+
+    // 3
+    printf("\n\t    *\n");
+    printf("\t   ***\n");
+    printf("\t  *****\n");
+    printf("\t *******\n");
+    printf("\t  HH HH\n");
+    printf("\t  ZZZZZ");
+
+    return 0;
+}
